cf3fbf7dcc1766ca034de4fe8d8e1505ebf32741
Edo <1122140027@global.ac.id>
2024-04-24 19:38:09 +0700

    fix /items

--- app/(user)/items/page.tsx
+++ app/(user)/items/page.tsx
@@ -5,16 +5,27 @@ import { getCurrentUser } from '@/services/user'
 
 const ItemsPage = async () => {
   const allBorrows = await getAllBorrowed()
-  const approvedBorrows = await getAllBorrowedApproved()
-  const notApprovedBorrows = await getAllBorrowedNotApproved()
-  const returnedBorrows = await getAllBorrowedReturned()
-  const notReturnedBorrows = await getAllBorrowedNotReturned()
-  const userAllBorrows = await getAllBorrowedUser()
-  const userApprovedBorrows = await getAllBorrowedUserApproved()
-  const userNotApprovedBorrows = await getAllBorrowedUserNotApproved()
-  const userReturnedBorrows = await getAllBorrowedUserReturned()
-  const userNotReturnedBorrows = await getAllBorrowedUserNotReturned()
+  const approvedBorrows = allBorrows.filter((borrow) => borrow.approved === true)
+  const notApprovedBorrows = allBorrows.filter((borrow) => borrow.approved === false)
+  const returnedBorrows = allBorrows.filter((borrow) => borrow.isReturned === true)
+  const notReturnedBorrows = allBorrows.filter((borrow) => borrow.isReturned === false)
+  // const approvedBorrows = await getAllBorrowedApproved()
+  // const notApprovedBorrows = await getAllBorrowedNotApproved()
+  // const returnedBorrows = await getAllBorrowedReturned()
+  // const notReturnedBorrows = await getAllBorrowedNotReturned()
+
   const currentUser = await getCurrentUser()
+
+  const userAllBorrows = await getAllBorrowedUser()
+
+  const userApprovedBorrows = userAllBorrows.filter((borrow) => borrow.approved === true)
+  const userNotApprovedBorrows = userAllBorrows.filter((borrow) => borrow.approved === false)
+  const userReturnedBorrows = userAllBorrows.filter((borrow) => borrow.isReturned === true)
+  const userNotReturnedBorrows = userAllBorrows.filter((borrow) => borrow.isReturned === false)
+  // const userApprovedBorrows = await getAllBorrowedUserApproved() 
+  // const userNotApprovedBorrows = await getAllBorrowedUserNotApproved()
+  // const userReturnedBorrows = await getAllBorrowedUserReturned()
+  // const userNotReturnedBorrows = await getAllBorrowedUserNotReturned()
 
 
   return (
